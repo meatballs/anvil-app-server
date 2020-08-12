ab324a162dfa97b865aceae6005ba45f26819a8d
Owen Campbell <owen.campbell@tanti.org.uk>
2020-08-12 09:40:34 +0100

    Fix permissions problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 export LD_PRELOAD='/usr/lib/libnss_wrapper.so'
+export USER_ID=${USER_ID:=1000}
+export GROUP_ID=${GROUP_ID:-$USER_ID}
 envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
 
+chown -R $USER_ID:$GROUP_ID /anvil-data
 exec gosu $USER_ID:$GROUP_ID $@
